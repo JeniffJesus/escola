8038f06d0ae93d36953abc09a0f1471c41e37b86
JeniffJesus <jeniffer14jesus@gmail.com>
2024-05-10 10:51:36 +0100

    Add files via upload

---
+++ Aventura do saber/questions.js
@@ -0,0 +1,275 @@
+const QUESTIONS = [
+    {
+        "question": "Uma das características do mercado de concorrência perfeita é a de os produtos não apresentarem diferenças significativas entre eles.Esta característica denomina-se...",
+        "answers": [
+            {
+                "text": "homogeneidade",
+                "correct": true
+            },
+            {
+                "text": "atomicidade",
+                "correct": false
+            },
+            {
+                "text": "transparência",
+                "correct": false
+            },
+            {
+                "text": "permeabilidade",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Se o preço do bem A aumentar e se esse facto for o único responsável pelo aumento da procura do bem B, podemos afirmar que...",
+        "answers": [
+            {
+                "text": "... B é substituto de A",
+                "correct": true
+            },
+            {
+                "text": "... B é complementar de A",
+                "correct": false
+            },
+            {
+                "text": "... A é independente de B",
+                "correct": false
+            },
+            {
+                "text": "... A é inferior a B",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Segundo a lei da oferta...",
+        "answers": [
+            {
+                "text": "... a quantidade oferecida de um bem aumenta quando o seu preço aumenta.",
+                "correct": true
+            },
+            {
+                "text": "... a quantidade oferecida de um bem aumenta quando o seu preço diminui.",
+                "correct": false
+            },
+            {
+                "text": "... o preço de um bem aumenta quando a quantidade oferecida desse bem aumenta.",
+                "correct": false
+            },
+            {
+                "text": "... o preço de um bem aumenta quando a quantidade oferecida desse bem diminui.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Um mercado de monopólio caracteriza-se pela existência de...",
+        "answers": [
+            {
+                "text": "... um vendedor e muitos compradores.",
+                "correct": true
+            },
+            {
+                "text": "... muitos vendedores e alguns compradores.",
+                "correct": false
+            },
+            {
+                "text": "... alguns vendedores e um comprador.",
+                "correct": false
+            },
+            {
+                "text": "... muitos vendedores e um comprador.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Numa situação de oligopólio...",
+        "answers": [
+            {
+                "text": "... existe um número reduzido de vendedores.",
+                "correct": true
+            },
+            {
+                "text": "... a procura é superior à oferta.",
+                "correct": false
+            },
+            {
+                "text": "... a oferta é superior à procura.",
+                "correct": false
+            },
+            {
+                "text": "... existe um único vendedor.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Verifica-se que a procura do bem Y aumenta em resultado do aumento do preço do bem Z (mantendo-se tudo o resto constante). Então, pode concluir-se que...",
+        "answers": [
+            {
+                "text": "Z e Y são bens substitutos.",
+                "correct": true
+            },
+            {
+                "text": "Y é um bem de primeira necessidade.",
+                "correct": false
+            },
+            {
+                "text": "Z é um bem inferior.",
+                "correct": false
+            },
+            {
+                "text": "Z e Y são bens complementares.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "O mercado de certo bem é um mercado de concorrência perfeita. Então, nesse mercado, existem...",
+        "answers": [
+            {
+                "text": "muitos compradores e muitos vendedores.",
+                "correct": true
+            },
+            {
+                "text": "poucos compradores e muitos vendedores.",
+                "correct": false
+            },
+            {
+                "text": "poucos compradores e poucos vendedores.",
+                "correct": false
+            },
+            {
+                "text": "muitos compradores e poucos vendedores.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Em certo momento, verificou-se o aumento da oferta do bem D. Um fator que pode explicar esse aumento é...",
+        "answers": [
+            {
+                "text": "o aumento da produtividade na produção do bem D.",
+                "correct": true
+            },
+            {
+                "text": "a diminuição do preço do bem D.",
+                "correct": false
+            },
+            {
+                "text": "o aumento do salário dos trabalhadores que produzem o bem D.",
+                "correct": false
+            },
+            {
+                "text": "a diminuição da procura do bem D.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Suponha que o preço dos combustíveis aumentou num dado momento. Então, permanecendo tudo o resto constante, é previsível...",
+        "answers": [
+            {
+                "text": "a diminuição da procura de automóveis.",
+                "correct": true
+            },
+            {
+                "text": "o aumento da procura de pneus.",
+                "correct": false
+            },
+            {
+                "text": "o aumento da oferta de automóveis.",
+                "correct": false
+            },
+            {
+                "text": "a diminuição da oferta de bicicletas.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "A homogeneidade do produto é um dos pressupostos do mercado de concorrência perfeita. Este pressuposto significa que",
+        "answers": [
+            {
+                "text": "os bens vendidos nesse mercado têm, para os consumidores, características iguais.",
+                "correct": true
+            },
+            {
+                "text": "as empresas que atuam nesse mercado têm todas a mesma dimensão.",
+                "correct": false
+            },
+            {
+                "text": "os consumidores, nesse mercado, adotam todos o mesmo comportamento.",
+                "correct": false
+            },
+            {
+                "text": "as mercadorias vendidas nesse mercado têm sempre o mesmo preço.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Quando afirmamos que um determinado mercado é de concorrência perfeita, tal significa que se verificam certas características, nomeadamente, o facto de",
+        "answers": [
+            {
+                "text": "o número de empresas a operar no mercado ser elevado.",
+                "correct": true
+            },
+            {
+                "text": "o produto transacionado no mercado ser diferenciado.",
+                "correct": false
+            },
+            {
+                "text": "o preço a que o bem é vendido no mercado ser baixo.",
+                "correct": false
+            },
+            {
+                "text": "o acesso a esse mercado estar sujeito a barreiras.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Num mercado de concorrência perfeita, mantendo-se tudo o resto constante, a lei da procura traduz",
+        "answers": [
+            {
+                "text": "a diminuição da quantidade procurada quando o preço do bem aumenta.",
+                "correct": true
+            },
+            {
+                "text": "a diminuição da quantidade procurada quando o preço do bem diminui.",
+                "correct": false
+            },
+            {
+                "text": "o aumento da quantidade procurada quando o rendimento das Famílias aumenta.",
+                "correct": false
+            },
+            {
+                "text": "o aumento da quantidade procurada quando o rendimento das Famílias diminui.",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Um oligopólio é uma estrutura do mercado em que existe",
+        "answers": [
+            {
+                "text": "um número reduzido de vendedores, com capacidade de influenciar o preço de mercado.",
+                "correct": true
+            },
+            {
+                "text": "um número muito elevado de vendedores de um produto diferenciado.",
+                "correct": false
+            },
+            {
+                "text": "um número muito elevado de vendedores de um produto homogéneo.",
+                "correct": false
+            },
+            {
+                "text": "um número reduzido de vendedores, sem capacidade de influenciar o preço de mercado.",
+                "correct": false
+            }
+        ]
+    }
+];
